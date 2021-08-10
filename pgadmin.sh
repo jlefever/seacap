ec8b008f509a99009fbd45d77f4f3c321c85df26
Jason Lefever <jtl86@drexel.edu>
2021-08-10 15:51:17 -0400

    Update pgadmin

--- pgadmin.sh
+++ pgadmin.sh
@@ -1,1 +1,1 @@
-docker run --rm --network=host -e "PGADMIN_DEFAULT_EMAIL=me@example.org" -e "PGADMIN_DEFAULT_PASSWORD=password" -e "PGADMIN_LISTEN_PORT=8090" dpage/pgadmin4
+docker run --rm --network=host -e "PGADMIN_DEFAULT_EMAIL=me@example.org" -e "PGADMIN_DEFAULT_PASSWORD=password" -e "PGADMIN_LISTEN_PORT=8090" dpage/pgadmin4:5.5
