1cedaca06a651945264e27be390b491612f90cd3
Jason Lefever <jtl86@drexel.edu>
2021-10-08 16:39:35 -0400

    Get started with JGit

--- seacap-extractor/src/main/java/net/jlefever/seacap/App.java
+++ seacap-extractor/src/main/java/net/jlefever/seacap/App.java
@@ -16,6 +16,13 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.diff.DiffAlgorithm;
+import org.eclipse.jgit.diff.DiffFormatter;
+import org.eclipse.jgit.diff.HistogramDiff;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.util.io.DisabledOutputStream;
 import org.sql2o.Sql2o;
 
 import net.jlefever.seacap.churn.Change;
@@ -42,7 +49,7 @@ import net.jlefever.seacap.git.GitDriver;
 
 public class App
 {
-    public static void main(String[] args) throws IOException, InterruptedException
+    public static void main(String[] args) throws IOException, InterruptedException, GitAPIException
     {
         var options = new Options();
 
@@ -57,12 +64,11 @@ public class App
         var parser = new DefaultParser();
         var formatter = new HelpFormatter();
         CommandLine cmd = null;
-        
+
         try
         {
             cmd = parser.parse(options, args);
-        }
-        catch (ParseException e)
+        } catch (ParseException e)
         {
             System.out.println(e.getMessage());
             formatter.printHelp("seacap-extractor", options);
@@ -74,7 +80,7 @@ public class App
 
         // Setup Git
         var cacheDir = Paths.get(System.getProperty("user.home"), ".seacap").toString();
-        var git = new GitDriver("git", cacheDir);
+        var driver = new GitDriver("git", cacheDir);
 
         // Load project yaml
         var project = Project.loadFromYaml(new FileInputStream(new File(ymlFilePath)));
@@ -83,77 +89,80 @@ public class App
 
         // Clone project
         System.out.println("Cloning repository...");
-        var repo = git.clone(project.getGit());
+        var git = driver.clone(project.getGit(), project.getGitLeadRef());
+        var repo = git.getRepository();
+        var repoDir = repo.getDirectory().getParentFile().getAbsolutePath();
+        doSomething(repo, project.getGitLeadRef());
 
-        // Collect historical information
-        System.out.println("Fetching historical information...");
-        var flatChanges = new GetChanges().call(repo.getDir(), leadRef, pathFilter, project.getGitMaxCommits());
-        var builder = new TreeTagBuilder();
-        flatChanges.forEach(c -> builder.add(c.getTag()));
-        var changes = flatChanges.stream().map(c -> new ChangeImpl<>(builder.add(c.getTag()), c.getRev(), c.getChurn()))
-                .collect(toList());
-        builder.build();
-        var entities = builder.getRoots();
+        // // Collect historical information
+        // System.out.println("Fetching historical information...");
+        // var flatChanges = new GetChanges().call(repoDir, leadRef, pathFilter, project.getGitMaxCommits());
+        // var builder = new TreeTagBuilder();
+        // flatChanges.forEach(c -> builder.add(c.getTag()));
+        // var changes = flatChanges.stream().map(c -> new ChangeImpl<>(builder.add(c.getTag()), c.getRev(), c.getChurn()))
+        //         .collect(toList());
+        // builder.build();
+        // var entities = builder.getRoots();
 
-        // Database
-        clean(dbFilePath);
-        var db = new Sql2o("jdbc:sqlite:" + dbFilePath, null, null);
+        // // Database
+        // clean(dbFilePath);
+        // var db = new Sql2o("jdbc:sqlite:" + dbFilePath, null, null);
 
-        // Create tables
-        System.out.println("Creating tables...");
-        try (var con = db.open())
-        {
-            new CreateEntityTableTask().prepare(con).executeUpdate();
-            new CreateCommitTableTask().prepare(con).executeUpdate();
-            new CreateChangeTableTask().prepare(con).executeUpdate();
-            new CreateDepTableTask().prepare(con).executeUpdate();
-            new CreateMetaTableTask().prepare(con).executeUpdate();
-        }
+        // // Create tables
+        // System.out.println("Creating tables...");
+        // try (var con = db.open())
+        // {
+        //     new CreateEntityTableTask().prepare(con).executeUpdate();
+        //     new CreateCommitTableTask().prepare(con).executeUpdate();
+        //     new CreateChangeTableTask().prepare(con).executeUpdate();
+        //     new CreateDepTableTask().prepare(con).executeUpdate();
+        //     new CreateMetaTableTask().prepare(con).executeUpdate();
+        // }
 
-        // Create batch runner
-        var runner = new BatchTaskRunner(db);
+        // // Create batch runner
+        // var runner = new BatchTaskRunner(db);
 
-        // Insert historical entities
-        System.out.println("Inserting historical entities...");
-        var entityIds = new IdMapImpl<TreeTag>();
-        runner.run(new BatchEntityInsertTask(entityIds), entities);
+        // // Insert historical entities
+        // System.out.println("Inserting historical entities...");
+        // var entityIds = new IdMapImpl<TreeTag>();
+        // runner.run(new BatchEntityInsertTask(entityIds), entities);
 
-        // Insert commits
-        System.out.println("Inserting commits...");
-        var commits = changes.stream().map(c -> c.getRev()).collect(toSet());
-        var commitIds = new IdMapImpl<String>();
-        runner.run(new BatchCommitInsertTask(commitIds), commits);
+        // // Insert commits
+        // System.out.println("Inserting commits...");
+        // var commits = changes.stream().map(c -> c.getRev()).collect(toSet());
+        // var commitIds = new IdMapImpl<String>();
+        // runner.run(new BatchCommitInsertTask(commitIds), commits);
 
-        // Insert changes
-        System.out.println("Inserting changes...");
-        var changeIds = new IdMapImpl<Change<TreeTag>>();
-        runner.run(new BatchChangeInsertTask(changeIds, entityIds, commitIds), changes);
+        // // Insert changes
+        // System.out.println("Inserting changes...");
+        // var changeIds = new IdMapImpl<Change<TreeTag>>();
+        // runner.run(new BatchChangeInsertTask(changeIds, entityIds, commitIds), changes);
 
-        // Fetch current entities
-        System.out.println("Fetching current entities...");
-        git.checkout(repo, leadRef);
-        var paths = git.lsFiles(repo, pathFilter);
-        var builder2 = new GetTags("ctags").call(repo.getDir(), paths);
+        // // Fetch current entities
+        // System.out.println("Fetching current entities...");
+        
+        // var paths = driver.lsFiles(repoDir, pathFilter);
+        // var builder2 = new GetTags("ctags").call(repoDir, paths);
 
-        // Insert current entities
-        System.out.println("Inserting current entities...");
-        runner.run(new BatchEntityInsertTask(entityIds), builder2.getRoots());
+        // // Insert current entities
+        // System.out.println("Inserting current entities...");
+        // runner.run(new BatchEntityInsertTask(entityIds), builder2.getRoots());
 
-        // Insert line numbers
-        System.out.println("Inserting line numbers...");
-        runner.run(new BatchLinenoUpdateTask(entityIds), builder2.getTreeTags());
+        // // Insert line numbers
+        // System.out.println("Inserting line numbers...");
+        // runner.run(new BatchLinenoUpdateTask(entityIds), builder2.getTreeTags());
 
-        // Fetch dependencies
-        System.out.println("Fetching dependencies...");
-        var deps = new GetDepsFromDepends().call(repo.getDir(), "java", paths);
+        // // Fetch dependencies
+        // System.out.println("Fetching dependencies...");
+        // var deps = new GetDepsFromDepends().call(repoDir, "java", paths);
 
-        // Insert dependencies
-        System.out.println("Inserting dependencies...");
-        runner.run(new BatchDepInsertTask(entityIds, builder2.getTreeTags()), deps.entrySet());
+        // // Insert dependencies
+        // System.out.println("Inserting dependencies...");
+        // runner.run(new BatchDepInsertTask(entityIds, builder2.getTreeTags()), deps.entrySet());
 
-        // Insert project info
-        System.out.println("Inserting project info...");
-        runner.run(new BatchMetaInsertTask(), Arrays.asList(project));
+        // // Insert project info
+        // System.out.println("Inserting project info...");
+        // runner.run(new BatchMetaInsertTask(), Arrays.asList(project));
     }
 
     private static void clean(String dir)
@@ -162,5 +171,51 @@ public class App
         {
             new File(dir).delete();
         }
+    }
+
+    private static void doSomething(Repository repo, String branch) throws IOException
+    {
+        var ref = repo.getRefDatabase().findRef(branch);
+        var objectId = ref.getLeaf().getObjectId();
+        var walk = new RevWalk(repo);
+        walk.markStart(walk.parseCommit(objectId));
+
+        var formatter = new DiffFormatter(DisabledOutputStream.INSTANCE);
+        formatter.setRepository(repo);
+        formatter.setDetectRenames(false);
+        formatter.setDiffAlgorithm(new HistogramDiff());
+
+        for (var commit : walk)
+        {
+            if (commit.getParentCount() != 1)
+            {
+                continue;
+            }
+
+            var tree = commit.getTree();
+            var parent = commit.getParents()[0];
+            var parentTree = parent.getTree();
+
+            var files = formatter.scan(parentTree, tree);
+            
+            System.out.println(commit.getId());
+
+            for (var file : files)
+            {
+                System.out.println(file);
+                var header = formatter.toFileHeader(file);
+                var editList = header.toEditList();
+
+                for (var edit : editList)
+                {
+                    System.out.println(edit);
+                }
+            }
+
+            System.out.println();
+        }
+
+        formatter.close();
+        walk.close();
     }
 }
