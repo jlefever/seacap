f409b052ce33c0d7c279cf85b3f024cdd90187c8
Jason Lefever <jtl86@drexel.edu>
2021-10-22 17:47:05 -0400

    Remove unused import

--- seacap-extractor/src/main/java/net/jlefever/seacap/depends/GetDepsFromDepends.java
+++ seacap-extractor/src/main/java/net/jlefever/seacap/depends/GetDepsFromDepends.java
@@ -3,7 +3,6 @@ package net.jlefever.seacap.depends;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
 import depends.LangRegister;
